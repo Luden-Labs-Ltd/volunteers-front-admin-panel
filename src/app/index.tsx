8c15e37d11672b0f0fa78ff6f709c188745aa00c
bakuga <maximgancharik@mail.ru>
2026-03-03 15:27:05 +0300

    chore(test): test commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
-
-import { I18nProvider } from '../shared/lib/i18n';
+import { I18nProvider } from '@/shared/lib/i18n';
 import { QueryProvider } from './providers';
 import { Router } from './providers/router';
 
